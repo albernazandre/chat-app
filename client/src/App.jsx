a6fffac0afb7f7ae48c1848caeb3a334913c22d4
Andre <andrespeltzalbernaz@gmail.com>
2023-05-08 13:52:48 -0300

    projeto pronto

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Chat from "@/components/chat";
+import Login from "@/components/login";
+
+function App() {
+
+  return (
+    <div className='app'>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
